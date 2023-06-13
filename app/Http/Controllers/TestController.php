0d62283fa5208e9c4c9ac3b289bb239ba07f884a
akane-aoki <okaka3080annietomorrow@gmail.com>
2023-06-13 18:59:56 +0900

    Fix: DBからデータを取得してみた

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -4,14 +4,27 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Test;
+use Illuminate\Support\Facades\DB;
 
 class TestController extends Controller
 {
   public function index()
   {
+    // エロクアント
     $values = Test::all();
 
-    // dd($values);
+    $count = Test::count();
+
+    $first = Test::findOrFail(1);
+
+    $whereBBB = Test::where('Text', '=', 'bbb');
+
+    // クエリビルダ
+    $queryBuilder = DB::table('tests')->where('Text', '=', 'bbb')
+    ->select('id', 'text')
+    ->get();
+
+    dd($values, $count, $first, $whereBBB, $queryBuilder);
 
     return view('tests.test', compact('values'));
   }
